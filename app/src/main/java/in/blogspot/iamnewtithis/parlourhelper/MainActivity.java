1738f90cba0e233bb8e50d4c5e4b9558150ca589
Rahul Jain <talentediq@gmail.com>
2017-06-10 00:35:10 +0530

    This Patch adds Notification sound and spinner aka combobox to the UI with specific Data.
    TODO:
    Need to add specific time with in 40 minutes
    Notification sound should beep before 3 sec of completion and a long sound on finishing.

--- app/src/main/java/in/blogspot/iamnewtithis/parlourhelper/MainActivity.java
+++ app/src/main/java/in/blogspot/iamnewtithis/parlourhelper/MainActivity.java
@@ -1,18 +1,28 @@
 package in.blogspot.iamnewtithis.parlourhelper;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button StartTimer ;
     Button ResetTimer ;
 
     TextView Timer;
+
+    Spinner facial_spinner;
+
+
     public static int min = 0;
     public static int sec = 0;
 
@@ -21,12 +31,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+
         StartTimer = (Button)findViewById(R.id.StartTimer);
         ResetTimer = (Button)findViewById(R.id.ResetTimer);
 
         Timer = (TextView)findViewById(R.id.Timer);
+
+        facial_spinner = (Spinner)findViewById(R.id.facial_spinner);
+
+//      Fill Spinner with Values
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.facial_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        facial_spinner.setAdapter(adapter);
+        facial_spinner.setOnItemSelectedListener(this);
+
         final CountDownTimer[] start = new CountDownTimer[1];
 
+//      Timer event start.
         StartTimer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                         String str = Integer.toString(min) + ":" + Integer.toString(sec) ;
                         Timer.setText(str);
+
                     }
 
                     @Override
@@ -51,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                         sec = 0;
                         min = 0;
                         Timer.setText("00:00");
+                        toneGenerator.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 150);
                     }
                 }.start();
             }
@@ -68,4 +94,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView)view;
+        Toast.makeText(this, "You Selected "+myText.getText() , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
+
